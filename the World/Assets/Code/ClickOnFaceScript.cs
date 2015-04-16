7b5908ddbb0a85b6c6fedaddcca40b0249a87b13
Yiyi Wang <shd101wyy@sina.com>
2015-04-15 23:24:54 -0500

    begin to implement the World(temp) game in unity

---
+++ the World/Assets/Code/ClickOnFaceScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickOnFaceScript : MonoBehaviour {
+
+	public Vector3 delta; // I add this
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnMouseOver(){
+		// left mouse button is pressed
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log("Left click!");
+
+			// Destory the parent of the face we clicked
+			Destroy(this.transform.parent.gameObject);
+		}
+
+		// right mouse button is pressed
+		if (Input.GetMouseButtonDown (1)) {
+			Debug.Log("Right click!");
+
+			// call method from WorldGenerator class
+			// http://in2gpu.com/2014/08/09/build-minecraft-unity-part-3/
+			WorldGenerator.CloneAndPlace(this.transform.parent.transform.position + delta,  // N = C + delta
+			                             this.transform.parent.gameObject);    // The parent GameObject. we clone it
+		}
+	}
+}
---
+++ the World/Assets/Code/WorldGenerator.cs
@@ -0,0 +1,98 @@
+﻿/**
+ * 
+ * World generator using Perlin noise
+ * http://forum.unity3d.com/threads/tutorial-procedural-meshes-and-voxel-terrain-c.198651/
+ * http://alexstv.com/index.php/category/voxels
+ * http://in2gpu.com/category/game-development/unity/
+ * 
+ * 
+ * 
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class WorldGenerator : MonoBehaviour {
+	// Public fields are visible and their values can be changed dirrectly in the editor
+	
+	// Drag and drop here the Voxel from the Scene
+	// Used to create new instances
+	public GameObject Voxel;
+	
+	//Specify the dimensions of the world
+	public float SizeX;
+	public float SizeZ;
+	public float SizeY;
+
+	public GameObject FPSController;
+
+
+	// Use this for initialization
+	void Start () {
+		// Start the world generation coroutine
+		// StartCoroutine function always returns immediately, however you can yield the result. 
+		StartCoroutine (SimpleGenerator ()); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public static void CloneAndPlace(Vector3 newPosition, GameObject originalGameobject){
+		//clone
+		GameObject clone = (GameObject)Instantiate (originalGameobject, newPosition, Quaternion.identity);
+
+		// place 
+		clone.transform.position = newPosition; 
+
+		//rename 
+		clone.name = "Voxel@" + clone.transform.position;
+	}
+
+
+   /* from docs.unity.com
+	* The execution of a coroutine can be paused at any point using the yield statement. 
+	* The yield return value specifies when the coroutine is resumed. 
+	* Coroutines are excellent when modelling behaviour over several frames. 
+	* Coroutines have virtually no performance overhead. 
+	* StartCoroutine function always returns immediately, however you can yield the result. 
+	* This will wait until the coroutine has finished execution.
+	*/
+	IEnumerator SimpleGenerator()
+	{
+		// In this Coroutine we will instantiate 20 voxels per frame
+		uint numberOfInstances = 0;
+		uint instancesPerFrame = 20;
+		
+		for(int x = 1; x <= SizeX; x++)
+		{
+			for(int z = 1; z<= SizeZ; z++)
+			{
+				// Compute a random height
+				float height = 10*Mathf.PerlinNoise(x/12.0f, z/12.0f); // Random.Range(0, SizeY);
+				for (int y = 0; y <= height; y++)
+				{
+					// Compute the position for every voxel
+					Vector3 newPosition = new Vector3(x, y, z);
+					// Call the method giving the new position and a Voxel instance as parameters
+					CloneAndPlace(newPosition, Voxel);
+					// Increment numberOfInstances
+					numberOfInstances++;
+					
+					// If the number of instances per frame was met
+					if(numberOfInstances == instancesPerFrame)
+					{
+						// Reset numberOfInstances
+						numberOfInstances = 0;
+						// Wait for next frame
+						yield return new WaitForEndOfFrame();
+					}
+				}
+			}
+		}
+
+		// enable gravity
+		//FPSController.GetComponent<Rigidbody>().useGravity = true;
+	}
+}
